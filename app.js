6efbf048ee4e0b79a8a76461a7cd94162b8121cb
maky.madingou <maky.madingou@wizall.com>
2021-01-18 01:10:40 +0000

    fix CORS

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const fileUpload = require("express-fileupload");
 const dbConfig = require("./app/config/db.config");
-const mongoose = require("mongoose");
 const db = {};
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-require("./app/routes/routes.js")(app);
 
 mongoose.Promise = global.Promise;
 db.mongoose = mongoose;
@@ -23,9 +23,17 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
 app.use(cors());
+app.use(fileUpload());
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+require("./app/routes/routes.js")(app);
 app.use(express.static("public"));
